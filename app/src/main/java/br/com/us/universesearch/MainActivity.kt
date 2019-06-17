3f34330817d1e217cb6eafeb047a01650caa2eb5
wesleyclzns <calazanscafe@outlook.com>
2019-06-17 20:34:10 -0300

    criacao de codigo activity

--- app/src/main/java/br/com/us/universesearch/MainActivity.kt
+++ app/src/main/java/br/com/us/universesearch/MainActivity.kt
@@ -2,11 +2,22 @@ package br.com.us.universesearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var url = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_ir.setOnClickListener {
+            url = edt_url.editableText.toString().trim()
+
+        }
+            webUniverse.settings.javaScriptEnabled = true
+            webUniverse.loadUrl(url)
     }
 }
